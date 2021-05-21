b86ddafc37a38c5d7713132649aebee90cd860e4
james <jamesodwuor4@gmail.com>
2021-05-21 22:48:28 +0300

    add checkyugioh

--- index.js
+++ index.js
@@ -7,3 +7,32 @@ function convertFahrToCelsius(temperature){
         return `${temperature} is not a valid number but a/an ${dataType}`;
     }
 }
+
+
+function checkYuGiOh(n){
+    if (isNaN(n)){
+        return (`invalid parameter: "${n}"`)
+    }
+    let arr = [];
+
+    for (i = 1; i<=n; i++){
+        if(i%2 === 0 && i %3 === 0 && i % 5 ===0){
+            arr.push("yu-gi-oh");
+        }else if(i%2 === 0 && i %3 === 0){
+            arr.push("yu-gi");
+        }else if(i %3 === 0 && i % 5 ===0){
+            arr.push("gi-oh");
+        } else if(i %2=== 0 && i % 5 ===0){
+            arr.push("yu-oh");
+        }else if(i % 2 === 0){
+            arr.push("yu");
+        }else if(i % 3 === 0){
+            arr.push("gi");
+        }else if(i % 5 === 0){
+            arr.push("oh");
+        }else{
+            arr.push(i)
+        }
+    }
+    return arr;
+}
